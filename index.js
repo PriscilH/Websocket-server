1df46f7e7439060347d92a407c3eede50d68ddc8
Priscilla <comptepro@mbpdepriscilla.home>
2023-03-26 14:22:57 +0200

    ecouter les connexions

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const WebSocket = require("ws");
 
 // Créer un serveur Websocket
 
-const wss = new WebSocket.Server();
+const wss = new WebSocket.Server({port: 8080}, () => {
+    console.log("Server started");
+});
+
+// Ecouter les connexions
+wss.on("connection", connection => {
+    // Ecouter les messages
+})
