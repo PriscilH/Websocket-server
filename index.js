6593510ec596b2f9c45fdf7cd2ccb862d6a9664e
Priscilla <comptepro@mbpdepriscilla.home>
2023-03-27 01:43:05 +0200

    edit index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ wss.on("connection", connection => {
     console.log("Someone is connected");
     // Ecouter les messages
     connection.on("message", message => {
+        // console.log(message)
         console.log(message.toString());
         // Transmettre un message à tous les autres utilisateurs connectés (broadcast)
         wss.clients.forEach(client => {
